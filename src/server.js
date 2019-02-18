e14c76982b74fa7c5f4ed3aad76b544c4325817f
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-02-18 17:05:48 +0100

    playing with middleware

--- src/server.js
+++ src/server.js
@@ -5,6 +5,12 @@ import cors from 'cors'
 
 export const app = express()
 
+const log = (req, res, next) => {
+  console.log('logging')
+  // do the 'next' bit of middleware
+  next()
+}
+
 app.disable('x-powered-by')
 
 app.use(cors())
@@ -12,4 +18,21 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+// this would have it run for the entire app
+// app.use(log)
+
+// pass middleware here as an arg
+app.get('/data', log, (req, res) => {
+  res.send({
+    message: 'hello'
+  })
+})
+
+// pass an array of middle wares to this route
+app.post('/data', [log, log], (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000)
+}
