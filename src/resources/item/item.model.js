fee2e1c2219e5d1e0592c6c032533f36e20a42bf
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-03-18 07:51:28 +0100

    tests passing for lesson 3

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,34 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+var ObjectId = mongoose.Schema.Types.ObjectId
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 50
+    },
+    status: {
+      default: 'active',
+      required: true,
+      type: String,
+      enum: ['active', 'complete', 'pastdue']
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      ref: 'user',
+      required: true,
+      type: ObjectId
+    },
+    list: {
+      ref: 'list',
+      required: true,
+      type: ObjectId
+    }
+  },
+  { timestamps: true }
+)
 export const Item = mongoose.model('item', itemSchema)
