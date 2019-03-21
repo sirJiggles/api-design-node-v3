eaf13d49ec3dac429f599c3a0992d4ac4f5a6e73
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-03-21 20:44:52 +0100

    lesson 4 tasks

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,4 +1,13 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+
+  const doc = await model.findOne({ _id: id, createdBy: userId }).exec()
+  if (!doc) {
+    return res.status(404).end()
+  }
+  return res.json({ data: doc })
+}
 
 export const getMany = model => async (req, res) => {}
 
